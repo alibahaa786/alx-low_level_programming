e7fee80f60ffe1e310ff0b888eb22bd90b9de14e
Ali <alibahaa786@gmail.com>
2023-06-16 07:56:23 -0700

    Done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits
+ * Return: 0 if executed properly
+ * otherwise non-zero
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar('0' + i);
+	putchar('\n');
+	return (0);
+}
