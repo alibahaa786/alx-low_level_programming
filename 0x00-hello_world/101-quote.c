bd9c597192add9b5c3bcb3bcdcb4932a15941d19
Ali <alibahaa786@gmail.com>
2023-06-16 05:23:20 -0700

    Quote Added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
  * to standard error
  * Return: 0 if exited properly, otherwise non-zero
