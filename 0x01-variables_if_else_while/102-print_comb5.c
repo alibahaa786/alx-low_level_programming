2fb8e1c982d60cc9f516bc765cfed2e86dbba67f
Ali <alibahaa786@gmail.com>
2023-06-16 19:23:12 -0700

    Done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * main - print combinations of
+ * double double digits
+ *Return: 0 if executed properly
+ *otherwise non-zero
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j == 9)
+			{
+				k = i + 1;
+				l = 0;
+			}
+			else
+			{
+				k = i;
+				l = j + 1;
+			}
+			for (k; k <= 9; k++)
+			{
+				for (l; l <= 9; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + l);
+					if (i + j + k + l != 35)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				l = 0;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
