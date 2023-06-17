2df38815983190366fbc9458275ee68a1ef276f3
Ali <alibahaa786@gmail.com>
2023-06-16 17:53:37 -0700

    Done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print double digits combination
+ * Return: 0 if executed properly
+ * otherwise non-zero
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			if (i != 8)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
