0d19be82397871803bdbb8319e1872a8b7843bf5
Ali <alibahaa786@gmail.com>
2023-06-20 17:48:39 -0700

    Done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -32,7 +32,7 @@ void print_to_98(int n)
 				unit = n % 10;
 				tens = n / 10;
 				if (tens > 9)
-					_putchar('0' + tens / 10)
+					_putchar('0' + tens / 10);
 				_putchar('0' + tens % 10);
 				_putchar('0' + unit);
 			}
@@ -52,11 +52,8 @@ void print_to_98(int n)
 			unit = n % 10;
 			tens = n / 10;
 			if (tens > 9)
-				int hundreds;
-				hundreds = tens / 10;
-				tens %= 10;
-				_putchar('0' + hundreds);
-			_putchar('0' + tens);
+				_putchar('0' + tens / 10);
+			_putchar('0' + tens % 10);
 			_putchar('0' + unit);
 			if (n != 98)
 			{
