616b156fa237017049e3150dca7bc4397614403b
Ali <alibahaa786@gmail.com>
2023-06-22 10:55:30 -0700

    DONE

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 -9
+ * Return: Null
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i<= 9; i++)
+		_putchar('0' + i);
+	_putchar('\n');
+}
