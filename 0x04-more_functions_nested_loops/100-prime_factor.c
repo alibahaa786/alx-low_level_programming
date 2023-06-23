6ec1f8fbfc72ffed9314bafe1769a35fb0e8c496
Ali <alibahaa786@gmail.com>
2023-06-22 18:16:05 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print largest prime number
+ * Return: 0
+*/
+
+int main(void)
+{
+	long i = 612852475143;
+	int div = 2;
+	int max;
+
+	while (i != 1)
+	{
+		if (i / div == 0)
+		{
+			i /= div;
+			max = div;
+		}
+		else
+		{
+			div += 1;
+		}
+	}
+	printf("%d", max);
+	return (0);
+}
