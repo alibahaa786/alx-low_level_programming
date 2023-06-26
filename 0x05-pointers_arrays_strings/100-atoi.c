592d258d9a40749e452a41e88a4b7a8e042dc5c2
Ali <alibahaa786@gmail.com>
2023-06-26 16:45:20 -0700

    done

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _atoi - print integer only
+ * @s: ppointer
+ * Return: integer
+*/
+
+int _atoi(char *s)
+{
+	int len;
+	int neg_pos;
+	int tens = 1;
+	int num_len = 0;
+	int number = 0;
+	int i;
+
+	neg_pos = 0;
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		if (s[len] == '-')
+			neg_pos--;
+		else if (s[len] == '+')
+			neg_pos++;
+		if (s[len] >= '0' and s[len] <= '9')
+		{
+			tens *= 10;
+			num_len++;
+		}
+	}
+	tens /= 10;
+	for (i = len - num_len; i < len; i++)
+	{
+		number += (s[len] - 48) * tens;
+		tens /= 10;
+	}
+	if (neg_pos < 0)
+		number *= -1;
+	return (number);
+}
