061809e449d4d685230ac2020ab7ae56d0eb9252
Ali <alibahaa786@gmail.com>
2023-06-26 09:22:25 -0700

    Done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,15 +10,14 @@ void rev_string(char *s)
 {
 	int len;
 	int i;
+	char *rev;
 
+	rev = "";
 	for (len = 0; s[len] != '\0'; len++)
 	{
 	}
-	char rev[len];
-
-	for (i = 0; len >= 0; i++)
+	while (len--)
 	{
-		len--;
-		rev[i] = s[len];
+		rev += s[len];
 	}
 }
