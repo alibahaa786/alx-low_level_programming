a720911f08c84e37a1ef3aee19b022887fbb0ddb
Ali <alibahaa786@gmail.com>
2023-06-26 11:54:06 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_hlaf - print last half
+ * @str: pointer
+ * Return: null
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	int len;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	len = i;
+	if (i % 2 == 0)
+	{
+
+		i /= 2;
+		for (; i < len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		i = i / 2 + 1;
+		for (; i < len; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
