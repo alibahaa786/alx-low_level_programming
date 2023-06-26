85b119e98645862722b86556ac8a84323b5d994c
Ali <alibahaa786@gmail.com>
2023-06-26 12:19:07 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: pointer
+ * @src: pointer
+ * Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (*dest);
+}
