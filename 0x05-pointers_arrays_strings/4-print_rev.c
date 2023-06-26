0307bffa04273b848bc99b12c67919bc7160a159
Ali <alibahaa786@gmail.com>
2023-06-26 09:03:31 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse string
+ * @s: pointer
+ * Return: null
+*/
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	for (len = 0; s[len] != '\0'; len++);
+	while (len--)
+		_putchar(s[len]);
+	_putchar('\n');
+}
