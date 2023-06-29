d62612a9b9b5643d336a5d1f3e586b1546b7426b
Ali <alibahaa786@gmail.com>
2023-06-29 16:13:53 -0700

    Done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int val_s1 = 0;
-	int val_s2 = 0;
 	int i;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		val_s1 += s1[i];
-	for (i = 0; s2[i] != '\0'; i++)
-		val_s2 += s2[i];
-	return (val_s1 - val_s2);
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			break;
+	}
+	return (s1[i] - s2[i]);
 }
