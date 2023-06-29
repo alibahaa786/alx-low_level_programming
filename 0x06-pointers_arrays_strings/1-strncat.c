d4fbee9a0ff739c184414a0456af1ebbe4984718
Ali <alibahaa786@gmail.com>
2023-06-29 15:23:36 -0700

    Done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,17 +12,19 @@ char *_strncat(char *dest, char *src, int n)
 	int len;
 	int i = 0;
 	int len_src;
+
 	for (len = 0; dest[len] != '\0'; len++)
 	{
 	}
 	for (len_src = 0; src[len_src] != '\0'; len_src++)
 	{
 	}
+	if (n > len_src)
+		n = len_src;
 	for (; i < n; i++)
 	{
 		dest[len] = src[i];
 		len++;
 	}
-	dest[len] = src[len_src];
 	return (dest);
 }
