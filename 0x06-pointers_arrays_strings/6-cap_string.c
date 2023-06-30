05f93d89b96a4c0c32983882f688447505876a80
Ali <alibahaa786@gmail.com>
2023-06-29 17:39:17 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - function
+ * @str: pointer
+ * Return: string
+*/
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 32 && str[i] <= 34) || str[i] == 40 || str[i] == 41 || str[i] == 44 || str[i] == 46 || str[i] == 59 || str[i] == 123 || str[i] == 125)
+		{
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] -= 32;
+		}
+	}
+	return (str);
+}
