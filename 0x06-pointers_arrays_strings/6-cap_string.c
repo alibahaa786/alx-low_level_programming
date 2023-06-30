b5e481d12331b090cc159379f7042f87ff1569c0
Ali <alibahaa786@gmail.com>
2023-06-29 17:40:32 -0700

    Done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,9 +14,10 @@ char *cap_string(char *str)
 	{
 		if ((str[i] >= 32 && str[i] <= 34) || str[i] == 40 || str[i] == 41 || str[i] == 44 || str[i] == 46 || str[i] == 59 || str[i] == 123 || str[i] == 125)
 		{
-			if (str[i] >= 97 && str[i] <= 122)
-				str[i] -= 32;
+			if (str[i - 1] >= 97 && str[i - 1] <= 122)
+				str[i - 1] -= 32;
 		}
 	}
+	str[0] -= 32;
 	return (str);
 }
