1411da3c7d23304060a5b807e29eecba536cbf83
Ali <alibahaa786@gmail.com>
2023-06-29 19:10:16 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - function
+ * @n: input
+ * Return: null
+*/
+
+void print_number(int n)
+{
+	unsigned int i;
+	
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	for (i = 1; n / i > 9; i *= 10)
+	{
+	}
+	i /= 10;
+	for (; i != 1; i /= 10)
+	{
+		_putchar('0' + n / i);
+		n -= (n / i) * i;
+	}
+	_putchar('0' + n);
+}
