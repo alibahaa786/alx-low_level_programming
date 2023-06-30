1d19d92767c6fb1fce27f009c5d1c02125841cef
Ali <alibahaa786@gmail.com>
2023-06-29 17:15:30 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - functioni
+ * @str: pointer
+ * Return: string
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	}
+	return (str);
+}
