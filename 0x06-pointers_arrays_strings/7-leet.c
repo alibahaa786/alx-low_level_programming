2e9c12b0ca6543e6377a250839ee728e4aad0a72
Ali <alibahaa786@gmail.com>
2023-06-29 18:21:04 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - function
+ * @str: pointer
+ * Return: string
+*/
+
+char *leet(char *str)
+{
+	char str2[] = {'0' , 'L', '!', 'E', 'A', '!', '!', 'T'};
+	int i;
+	int j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (str[i] == str2[j] || str[i] - 32 == str2[j])
+				str[i] = str[j];
+		}
+	}
+	return (str);
+}
