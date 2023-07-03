df402ccaa221ffcf92c37e2506624c18c8c1aa37
Ali <alibahaa786@gmail.com>
2023-07-03 07:37:19 -0700

    Done

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - function
+ * @c: pointer
+ * @accept: pointer
+ * Return: int
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int check;
+	int result = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				check++;
+		}
+		if (check)
+			result++;
+		else
+			break;
+	}
+	return (result);
+}
