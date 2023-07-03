76c3a002f1510a606ba8228c713134370b3dc753
Ali <alibahaa786@gmail.com>
2023-07-03 07:17:19 -0700

    Done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - function
+ * @s: pointers
+ * @c: input
+ * Return: char
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s[i]);
+	}
+	return (NULL);
+}
