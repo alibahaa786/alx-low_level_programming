31cb9a0b8520df0e10a584cac88f0065a374a1ba
Ali <alibahaa786@gmail.com>
2023-07-03 07:43:38 -0700

    Done

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function
+ * @s: pointer
+ * @accept: pointer
+ * Return: char
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				s += i;
+				return (s);
+			}
+		}
+	}
+	return ('\0');
+}	
