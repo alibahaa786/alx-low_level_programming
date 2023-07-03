5f3a70e0bb574ba0d0bbd4367d13b66f8831feeb
Ali <alibahaa786@gmail.com>
2023-07-03 09:21:58 -0700

    Done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,10 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; index++)
 	{
 		if (s[i] == c)
-		{
-			s += i;
-			return (s);
-		}
+			return (s + index);
 	}
 	return ('\0');
 }
