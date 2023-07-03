cb64690833fbebd0d78cc7222b021bc5f12de0ac
Ali <alibahaa786@gmail.com>
2023-07-03 06:53:13 -0700

    Done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memset - function
+ * @s: input
+ * @b: input
+ * @n: input
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+		s[n] = &b;
+	return (s);
+}
