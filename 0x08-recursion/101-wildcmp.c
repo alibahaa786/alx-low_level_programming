0d4c14d52e6ff4f837f4c91a64b036a04fd5e32f
Ali <alibahaa786@gmail.com>
2023-07-05 12:47:03 -0700

    Done

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _compare - function
+ * @s1: string
+ * @s2: string
+ * @i1: counter s1
+ * @i2: counter s2
+ * @star_present: boolean
+ * Return: 0 or 1
+*/
+
+int _compare(char *s1, char *s2, int i1, int i2, int star_present)
+{
+	if (!s1[i1] && !s2[i2])
+		return (1);
+	if (s2[i2] == '*')
+	{
+		star_present = 1;
+		return (_compare(s1, s2, i1, i2 + 1));
+	}
+	if (!star_present)
+	{
+		if (s1[i1] == s2[i1])
+			return (_compare(s1, s2, i1 + 1, i2, 0));
+		return (0);
+	}
+	if (star_present)
+	{
+		if (s1[i1] == s2[i2])
+			return (_compare(s1, s2, i1 + 1, i2 + 2, 1));
+		return (_compare(s1, s2, i1 + 1, i2, 1));
+	}
+}
+
+/**
+ * wildcmp - function
+ * @s1: string
+ * @s2: string
+ * Return: 1 or 0
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	return (_compare(s1, s2, 0, 0, 0));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,5 @@ int find_root(int n, int root);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int helper_palindrome (char *s, int len, int i);
+int wildcmp(char *s1, char *s2);
+int _compare(char *s1, char *s2, int i1, int i2, int star_present);
