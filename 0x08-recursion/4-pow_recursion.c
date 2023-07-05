f149952cc23ab950ed867be8f3ac8ab0f34c6af9
Ali <alibahaa786@gmail.com>
2023-07-05 06:51:15 -0700

    Done

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function
+ * @x: int
+ * @y: int
+ * Return: result
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y - 1)
+		return (x * _pow_recursion(x, y - 1);
+	else
+		return (x);
