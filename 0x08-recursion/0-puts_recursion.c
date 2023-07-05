60b5223dba4979296cd03442ddf18701142a596e
Ali <alibahaa786@gmail.com>
2023-07-05 06:06:47 -0700

    Done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function
+ * @s: string
+ * Return: Null
+*/
+
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		s += 1;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\0');
+}
