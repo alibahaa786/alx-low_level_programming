401354e8bbe0ce68f9efa567073ee453fa7f1901
Ali <alibahaa786@gmail.com>
2023-07-05 06:36:42 -0700

    Done

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,10 +8,12 @@
 
 int factorial(int n)
 {
+	int result = 1;
+	
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	if (n > 0)
-		return (n + factorial(n-1));
+	result *= factorial(n-1);
+	return (result);
 }
