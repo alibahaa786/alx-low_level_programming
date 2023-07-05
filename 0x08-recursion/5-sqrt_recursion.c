3e8bfaf2657ae771c3d84c80d94b08845793df3d
Ali <alibahaa786@gmail.com>
2023-07-05 10:43:42 -0700

    Done

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * find_root - function
+ * @n: number
+ * @root: result
+ * Return: root
+*/
+
+int find_root(int n, int root)
+{
+	if (root * root == n)
+		return (root);
+	if (root >= n / 2)
+		return (-1);
+	find_root(n, root + 1);
+}
+
+/**
+ * _sqrt_recursion - function
+ * @n: input
+ * Return: result
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (find_root(n, 0));
+}
