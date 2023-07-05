858dc053d7f2dea8e01ae173310ffb07e40903b7
Ali <alibahaa786@gmail.com>
2023-07-05 11:24:51 -0700

    Done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,10 +1,25 @@
 #include "main.h"
 
 /**
+ * _str_len - function
+ * @s: string
+ * @len: string length
+ * Return: len
+*/
+
+int _str_len (char *s, int len)
+{
+	if (s[len])
+		return (_str_len(s, len + 1));
+	return (len);
+}
+
+/**
  * helper_palindrome - function
  * @s: string
  * @len: string length
  * @i: counter
+ * Return: 0 or 1
 */
 
 int helper_palindrome (char *s, int len, int i)
@@ -26,9 +41,7 @@ int is_palindrome(char *s)
 {
 	int len;
 
-	for (len = 0; s[len]; len++)
-	{
-	}
+	len = _str_len(s, 0);
 	if (len == 0 || len == 1)
 		return (1);
 	return (helper_palindrome(s, len, 0));
