e3ae4747af50acb0abf2148a643ee8607f467542
Ali <alibahaa786@gmail.com>
2023-07-07 09:26:43 -0700

    Done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: integer
+ * @argv: array
+ * Return: o
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
