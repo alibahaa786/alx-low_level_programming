da3f0b6b73fedb0a7501f29bc2a8b50fc456b266
Ali <alibahaa786@gmail.com>
2023-07-07 09:35:55 -0700

    Done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: integer
+ * @argv: array
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc);
+	return (0);
+}
