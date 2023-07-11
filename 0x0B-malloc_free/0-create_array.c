77983794836d7c3412f11ec8fc9851188ecec40e
Ali <alibahaa786@gmail.com>
2023-07-11 05:41:17 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,8 @@ char *create_array(unsigned int size, char c)
 	if (!size)
 		return ('\0');
 	array = malloc(sizeof(char) * size);
+	if (!array)
+		return ('\0');
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
