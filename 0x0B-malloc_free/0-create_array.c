1fd93765235b20eff68e6d28f8fabd97d1627acc
Ali <alibahaa786@gmail.com>
2023-07-10 19:09:34 -0700

    Done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_array - function
+ * @size: integer
+ * c: char
+ * Return: null
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+
+	if (!size)
+		return ('\0');
+	array = malloc(sizeof(c) * size);
+	while (size--)
+		array[size] = c;
+	return (array);
+}
