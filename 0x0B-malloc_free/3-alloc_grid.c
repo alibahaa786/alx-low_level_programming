f264efca8c59b424aa0c8709aad84b260045ed0a
Ali <alibahaa786@gmail.com>
2023-07-11 07:09:18 -0700

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,16 +17,24 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return ('\0');
-	*grid = malloc(sizeof(char) * height);
-	grid = malloc(sizeof(char) * width);
-	if (!grid || !*grid)
+	grid = malloc(sizeof(int *) * height);
+	if (!grid)
 		return ('\0');
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < width; j++)
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
 		{
-			*grid[i][j] = 0;
+			free(grid[i]);
+			while (i--)
+				free(grid[i]);
+			free(grid);
 		}
 	}
-	return (*grid);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
 }
