b801affa728ba487ce6ed6aec7c10bbf0a8a967f
Ali <alibahaa786@gmail.com>
2023-07-13 07:29:45 -0700

    Done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc-checked - function
+ * @b: integer
+ * Return: 0 or 98
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (!ptr)
+		return (98);
+	return (ptr);
+}
