6c2dee55d5051252bae13de6902a3af58df5978d
Ali <alibahaa786@gmail.com>
2023-07-13 14:46:30 -0700

    Done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,23 +8,23 @@
 
 int main(int argc, char *argv[])
 {
-	long long num1;
-	long long num2;
-	unsigned long long product;
+	long num1;
+	long num2;
+	unsigned long product;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoll(argv[1]);
-	num2 = atoll(argv[2]);
+	num1 = atol(argv[1]);
+	num2 = atol(argv[2]);
 	if (!num1 || !num2)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 	product = num1 * num2;
-	printf("%llu\n", product);
+	printf("%lu\n", product);
 	return (0);
 }
