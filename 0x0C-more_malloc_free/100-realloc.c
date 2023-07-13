23bcbdbdd836f770e65339da68e55adaa29e401b
Ali <alibahaa786@gmail.com>
2023-07-13 14:14:18 -0700

    Done

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,16 +2,19 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _realloc - function
+ * @ptr: array
+ * @old_size: int
+ * @new_size: inte
+ * Return: null
 */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *mem;
-	int i;
+	unsigned int i;
+	char *ptr_copy;
+	char *filler;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -28,10 +31,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	mem = malloc(new_size);
+	ptr_copy = ptr;
+	filler = mem;
 	if (!mem)
 		return (NULL);
 	for (i = 0; i < new_size && i < old_size; i++)
-		mem[i] = ptr[i];
+		filler[i] = *ptr_copy++;
 	free(ptr);
 	return (mem);
 }
