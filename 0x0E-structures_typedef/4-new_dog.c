c6563c6efccd313da431da99cb7aa3c4a022025e
Ali <alibahaa786@gmail.com>
2023-07-17 18:35:25 -0700

    Done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -64,8 +64,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = _strcpy(new_dog->name, name);
+	new_dog->name = name;
 	new_dog->age = age;
-	new_dog->owner = _strcpy(new_dog->owner, owner);
+	new_dog->owner = owner;
 	return (new_dog);
 }
