80d42ad95e1546a3257a0643bebf6c4abe246a4f
Ali <alibahaa786@gmail.com>
2023-07-19 13:27:39 -0700

    Done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "calc.h"
+#include "3-calc.h"
 #include <stdlib.h>
 
 /**
@@ -9,32 +9,30 @@
  * Return: 0 if successful
 */
 
-int main(argc, argv[])
+int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
+	char *op;
 
-	if (argc != 3)
+	if (argc != 4)
 	{
-		printf("Error\n");
+		printf("Erro 98r\n");
 		exit(98);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if (argv[2] != "+" &&
-			argv[2] != "-" &&
-			argv[2] != "/" &&
-			argv[2] != "*" &&
-			argv[2] != "%")
+	op = argv[2];
+	if (!get_op_func(op))
 	{
-		printf("Error\n");
+		printf("Error 99\n");
 		exit(99);
 	}
-	if ((argv[2] == "%" || argv[2] == "/") && argv[3] == 0)
+	if ((*op == 37 || *op == 47) && num2 == 0)
 	{
-		printf("Error\n");
+		printf("Error 100\n");
 		exit(100);
 	}
-	printf("%d\n",get_op_func(argv[2])(num1, num2));
+	printf("%d\n",get_op_func(op)(num1, num2));
 	return (0);
 }
