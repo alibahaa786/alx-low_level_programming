2ba10e295aea72fc61f75330810c54f7f4b5b4b6
Ali <alibahaa786@gmail.com>
2023-07-20 04:35:12 -0700

    Done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,6 +2,13 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - main
+ * @n: int
+ * @...: sdf
+ * Return: 0
+*/
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list numbers;
