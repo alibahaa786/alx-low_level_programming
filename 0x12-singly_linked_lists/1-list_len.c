f317851dbeded6f6f3dc6962800739832de559fa
Ali <alibahaa786@gmail.com>
2023-07-27 08:07:43 -0700

    Done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,17 @@
 #include "lists.h"
 
+/**
+ * list_len - function
+ * @h: node
+ * Return: number of elements
+*/
 size_t list_len(const list_t *h)
 {
 	int num = 0;
 
 	if (!h)
 		return (0);
-	num ++;
+	num++;
 	if (h->next)
 		num += list_len(h->next);
 	return (num);
