679b75cf9f7e853a491c2d771bc6abb1b8629408
Ali <alibahaa786@gmail.com>
2023-08-01 18:10:56 -0700

    Done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,10 +13,10 @@ void free_listint2(listint_t **head)
 
 	if (!head)
 		return;
-	while (head)
+	while (*head)
 	{
 		temp = (*head)->next;
-		free(head);
+		free(*head);
 		*head = temp;
 	};
 	head = NULL;
