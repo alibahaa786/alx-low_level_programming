00799d929fc82f22b6053c9e12209670526b3312
Ali <alibahaa786@gmail.com>
2023-08-01 17:56:01 -0700

    Done

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,17 @@
 #include "lists.h"
 
+/**
+ * listint_len - function
+ * @h: struct
+ * Return: num
+*/
+
 size_t listint_len(const listint_t *h)
 {
 	int sum = 1;
 
+	if (!h)
+		return (0);
 	if (h->next)
 		sum += listint_len(h->next);
 	return (sum);
