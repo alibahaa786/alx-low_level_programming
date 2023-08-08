7e694efaf1326ec76f00fbd9202afea61334c94a
Ali <alibahaa786@gmail.com>
2023-08-07 19:05:34 -0700

    Done:

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buffer = create_buf(argv[2]);
+	buf = create_buf(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buf, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
